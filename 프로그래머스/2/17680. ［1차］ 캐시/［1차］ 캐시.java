5dd7896dd179b6b4daf42221ad8b8d76803f18fd
SeungminOh <139704503+seungminoh-dev@users.noreply.github.com>
2026-08-24 12:33:03 +0900

    [level 2] Title: [1차] 캐시, Time: 20.95 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -3,35 +3,28 @@ import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        Map<String,Integer> cache = new HashMap<>();
-        int searchIndex = 0;
         if (cacheSize==0)
-            return 5*cities.length;
-        while(searchIndex<cities.length){
-            String target = cities[searchIndex].toLowerCase();
-            if(cache.getOrDefault(target,-1)==-1){
-                // 교체 알고리즘
-                if(cache.size()>=cacheSize){
-                    int min = 99999999;
-                    String targetKey = "";
-                    for(String key : cache.keySet()){
-                        if (cache.get(key)<min){
-                            targetKey = key;
-                            min = cache.get(key);
-                        }
-                    }
-                    cache.remove(targetKey); 
-                }
-                cache.put(target,searchIndex+1);
-                answer+=5;
-                searchIndex++;
-            }else{
+            return 5 * cities.length;
+        Map<String,Boolean> cache = new LinkedHashMap<>(16,0.75f,true);
+        int eleCount = 0;
+        for(int i=0;i<cities.length;i++){
+            //대소문자 정규화
+            String city = cities[i].toLowerCase();
+            //Cache Hit 검사
+            if (cache.get(city) != null){
                 answer+=1;
-                cache.put(target,searchIndex+1);
-                searchIndex++;
+            }else{
+                //eleCount = cacheSize 이면 제거하고 삽입 필요
+                if(cache.size() == cacheSize){
+                    String removeTargetKey = cache.keySet().iterator().next();
+                    eleCount--;
+                    cache.remove(removeTargetKey);
+                }
+                cache.put(city,true);
+                eleCount++;
+                answer+=5;
             }
         }
-        
         return answer;
     }
 }
